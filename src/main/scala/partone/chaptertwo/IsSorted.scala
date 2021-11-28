fe8a65f3e1b63c8a682a0aeebe201d0fdbb56018
Roy Straub <roy.straub@codesquad.nl>
2021-11-28 20:57:55 +0100

    Sorted with higher order function

---
+++ src/main/scala/partone/chaptertwo/IsSorted.scala
@@ -0,0 +1,24 @@
+package partone.chaptertwo
+
+import scala.annotation.tailrec
+
+object IsSorted {
+  def isSorted[A](array: Array[A], isOrdered: (A, A) => Boolean): Boolean = {
+    val size = array.length
+
+    @tailrec
+    def loop(currentIndex: Int, nextIndex: Int): Boolean = {
+      val current = array(currentIndex)
+      val next = array(nextIndex)
+
+      if (isOrdered(current, next)) {
+        if (nextIndex < size - 1)
+          loop(currentIndex + 1, nextIndex + 1)
+        else true
+      } else false
+    }
+
+    if (size <= 1) true
+    else loop(0, 1)
+  }
+}
---
+++ src/test/scala/partone/chaptertwo/IsSortedSpec.scala
@@ -0,0 +1,31 @@
+package partone.chaptertwo
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import partone.chaptertwo.IsSorted.isSorted
+
+class IsSortedSpec extends AnyFlatSpec with Matchers {
+  "is sorted" should "return true for a sorted array" in {
+    val sorted = Array(1, 2, 3, 4, 5)
+
+    isSorted(sorted, (x: Int, y: Int) => x < y) shouldBe true
+  }
+
+  it should "return false for an unsorted array" in {
+    val unsorted = Array(1, 2, 7, 5)
+
+    isSorted(unsorted, (x: Int, y: Int) => x < y) shouldBe false
+  }
+
+  it should "return true for any sorted array" in {
+    val sorted = Array("al", "alp", "alpa", "alpac", "alpaca")
+
+    isSorted(sorted, (a: String, b: String) => a.length < b.length) shouldBe true
+  }
+
+  it should "return false for any unsorted array" in {
+    val unsorted = Array("alpaca", "alpac", "alpa", "alp", "al")
+
+    isSorted(unsorted, (a: String, b: String) => a.length < b.length) shouldBe false
+  }
+}
