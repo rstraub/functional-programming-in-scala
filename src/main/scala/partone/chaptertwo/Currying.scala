09e0e5ef55844cdca3c45b6e32a5d2f89c205bb1
Roy Straub <roy.straub@codesquad.nl>
2021-11-29 22:01:40 +0100

    Cleaner

--- src/main/scala/partone/chaptertwo/Currying.scala
+++ src/main/scala/partone/chaptertwo/Currying.scala
@@ -1,5 +1,5 @@
 package partone.chaptertwo
 
 object Currying {
-  def curry[A, B, C](fn: (A, B) => C): A => B => C = (a: A) => (b: B) => fn(a, b)
+  def curry[A, B, C](fn: (A, B) => C): A => B => C = a => b => fn(a, b)
 }
