fb7185c9cd51ceebb24b7c280e80f17400268bba
Roy Straub <roy.straub@codesquad.nl>
2021-11-29 22:07:47 +0100

    Also uncurry

--- src/main/scala/partone/chaptertwo/Currying.scala
+++ src/main/scala/partone/chaptertwo/Currying.scala
@@ -2,4 +2,6 @@ package partone.chaptertwo
 
 object Currying {
   def curry[A, B, C](fn: (A, B) => C): A => B => C = a => b => fn(a, b)
+
+  def uncurry[A, B, C](fn: A => B => C): (A, B) => C = (a: A, b: B) => fn(a)(b)
 }
--- src/test/scala/partone/chaptertwo/CurrySpec.scala
+++ src/test/scala/partone/chaptertwo/CurrySpec.scala
@@ -5,10 +5,20 @@ import org.scalatest.matchers.should.Matchers
 
 class CurrySpec extends AnyFlatSpec with Matchers {
     "curry" should "return a partially applied function" in {
-        val initial = Currying.curry((a: Int, b: Int) => (a + b).toString)
+        val fn: (Int, Int) => String = (a: Int, b: Int) => (a + b).toString
+
+        val initial = Currying.curry(fn)
         val partial = initial(2)
         val result = partial(3)
 
         result shouldBe "5"
+    }
+
+    "uncurry" should "return a single function where all arguments must be supplied" in {
+        val fn = (a: Int) => (b: Int) => (a + b).toString
+
+        val result = Currying.uncurry(fn)
+
+        result(1, 2) shouldBe "3"
     }
 }
