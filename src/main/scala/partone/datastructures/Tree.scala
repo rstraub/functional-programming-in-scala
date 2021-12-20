40c45941eab0ca8258ad17d9f3d737f298065df4
Roy Straub <roy.straub@codesquad.nl>
2021-12-20 15:30:39 +0100

    Naive implementation for a branch

--- src/main/scala/partone/datastructures/Tree.scala
+++ src/main/scala/partone/datastructures/Tree.scala
@@ -2,7 +2,7 @@ package partone.datastructures
 
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
-case class Branch[A](left: Leaf[A], right: Leaf[A]) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 case object Trunk extends Tree[Nothing]
 
 object Tree {
@@ -10,6 +10,7 @@ object Tree {
 
 
   def apply[A](as: A*): Tree[A] = as match {
+    case as if as.size == 2 => Branch(apply(as.head), apply(as.tail.last))
     case as if as.size == 1 => Leaf(as.head)
     case _ => Trunk
   }
--- src/test/scala/partone/datastructures/TreeSpec.scala
+++ src/test/scala/partone/datastructures/TreeSpec.scala
@@ -12,6 +12,10 @@ class TreeSpec extends AnyFlatSpec with Matchers {
     Tree(1) shouldBe Leaf(1)
   }
 
+  it should "return branch and two leaves given two elements" in {
+    Tree(1, 2) shouldBe Branch(Leaf(1), Leaf(2))
+  }
+
   "size (ex 3.25)" should "count all nodes in the tree" ignore {
     Tree.size(Tree((1, 2), (3, 4))) shouldBe 7
   }
