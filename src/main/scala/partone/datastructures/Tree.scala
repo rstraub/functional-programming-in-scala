a8705e947202b422acc0ea38f7d5cd66a20a9504
Roy Straub <roy.straub@codesquad.nl>
2021-12-20 15:43:24 +0100

    Cheaty way

--- src/main/scala/partone/datastructures/Tree.scala
+++ src/main/scala/partone/datastructures/Tree.scala
@@ -1,5 +1,7 @@
 package partone.datastructures
 
+import scala.annotation.tailrec
+
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
@@ -8,9 +10,9 @@ case object Trunk extends Tree[Nothing]
 object Tree {
   def size[A](tree: Tree[A]): Int = 1
 
-
   def apply[A](as: A*): Tree[A] = as match {
-    case as if as.size == 2 => Branch(apply(as.head), apply(as.tail.last))
+    case as if as.size == 3 => Branch(apply(as.head, as(1)), apply(as(2)))
+    case as if as.size == 2 => Branch(apply(as.head), apply(as(1)))
     case as if as.size == 1 => Leaf(as.head)
     case _ => Trunk
   }
