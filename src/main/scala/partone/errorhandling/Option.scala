8cb89a0e4c417f8895910d84d155f0d99a329b3f
Roy Straub <roy.straub@codesquad.nl>
2021-12-21 17:24:00 +0100

    Ex 4.2: variance implementation using option flatmap

--- src/main/scala/partone/errorhandling/Option.scala
+++ src/main/scala/partone/errorhandling/Option.scala
@@ -8,19 +8,24 @@ sealed trait Option[+A] {
 
   def flatMap[B](fn: A => Option[B]): Option[B] = map(fn) getOrElse None
 
-  def filter(fn: A => Boolean): Option[A] = flatMap(a => if(fn(a)) Some(a) else None)
+  def filter(fn: A => Boolean): Option[A] = flatMap(a => if (fn(a)) Some(a) else None)
 
   def getOrElse[B >: A](default: => B): B = this match {
     case Some(v) => v
     case None => default
   }
 
-  def orElse[B >: A](option: => Option[B]): Option[B] = this map(Some(_)) getOrElse option
+  def orElse[B >: A](option: => Option[B]): Option[B] = this map (Some(_)) getOrElse option
 }
+
 case class Some[+A](value: A) extends Option[A]
+
 case object None extends Option[Nothing]
 
 object Option {
   def mean(xs: Seq[Double]): Option[Double] = if (xs.isEmpty) None else Some(xs.sum / xs.size)
-  def variance(xs: Seq[Double]): Option[Double] = None
+
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
 }
