9d0ae1c6ee8a7771afee853a4fb758ed5480a1f7
Roy Straub <roy.straub@codesquad.nl>
2021-12-22 14:24:17 +0100

    Ex 4.6: either flatMap

--- src/main/scala/partone/errorhandling/Either.scala
+++ src/main/scala/partone/errorhandling/Either.scala
@@ -5,7 +5,21 @@ sealed trait Either[+E, +A] {
     case Left(v) => Left(v)
     case Right(v) => Right(fn(v))
   }
+
+  def flatMap[EE >: E, B](fn: A => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Left(e) => Left(e)
+      case Right(v) => fn(v)
+    }
+
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing]
+
 case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+  def Try[A](a: => A): Either[Exception, A] = try Right(a) catch {
+    case e: Exception => Left(e)
+  }
+}
--- src/test/scala/partone/errorhandling/EitherSpec.scala
+++ src/test/scala/partone/errorhandling/EitherSpec.scala
@@ -2,6 +2,7 @@ package partone.errorhandling
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import partone.errorhandling.Either.Try
 
 class EitherSpec extends AnyFlatSpec with Matchers {
   "map (ex 4.6)" should "operate on right given value" in {
@@ -11,4 +12,14 @@ class EitherSpec extends AnyFlatSpec with Matchers {
   it should "return left given left" in {
     Left("boom").map((s: String) => s.toUpperCase()) shouldBe Left("boom")
   }
+
+  "flatMap" should "return new right given successful" in {
+    Right("1").flatMap(s => Try(s.toInt)) shouldBe Right(1)
+  }
+
+  it should "return new left given error" in {
+    Right("").flatMap(tryParse).isInstanceOf[Left[NumberFormatException]] shouldBe true
+  }
+
+  private def tryParse(s: String) = Try(s.toInt)
 }
