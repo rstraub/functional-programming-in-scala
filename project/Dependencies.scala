a347da8bf99af383d2bc01c5e420c4bed2675955
Roy Straub <roy.straub@codesquad.nl>
2021-12-30 15:46:27 +0100

    Config update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,6 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.10" % "test"
+  lazy val scalactic = "org.scalactic" %% "scalactic" % "3.2.10"
 }
