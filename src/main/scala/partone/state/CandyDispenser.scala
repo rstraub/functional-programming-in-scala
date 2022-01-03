74928a877efa84ad29994dc5949b64a3ef2571a7
Roy Straub <roy.straub@codesquad.nl>
2022-01-03 14:22:18 +0100

    Ex 6.11: update locked (cheaty way)

--- src/main/scala/partone/state/CandyDispenser.scala
+++ src/main/scala/partone/state/CandyDispenser.scala
@@ -8,7 +8,11 @@ case object Coin extends Input
 
 case class CandyDispenser(locked: Boolean = true, coins: Int = 0, candies: Int) {
   def simulate(inputs: List[Input]): State[CandyDispenser, (Int, Int)] = for {
-    _ <- modify[CandyDispenser](_ => CandyDispenser(locked, coins, candies))
+    _ <- modify[CandyDispenser](_ => update(inputs.head))
     s <- get
   } yield (s.coins, s.candies)
+
+  private def update(input: Input): CandyDispenser = {
+    CandyDispenser(false, coins, candies)
+  }
 }
