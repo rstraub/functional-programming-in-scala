2d3a2f9accc3b2c9d008514a193de793a612358d
Roy Straub <roy@codecraftr.nl>
2022-03-18 16:44:28 +0100

    Small styling issues

--- src/main/scala/partone/state/CandyDispenser.scala
+++ src/main/scala/partone/state/CandyDispenser.scala
@@ -8,17 +8,20 @@ case object Coin extends Input
 
 case object Turn extends Input
 
-case class CandyDispenser(locked: Boolean = true, coins: Int = 0, candies: Int) {
+case class CandyDispenser(locked: Boolean = true,
+                          coins: Int = 0,
+                          candies: Int) {
   private def update(input: Input): CandyDispenser =
     input match {
-      case Turn => CandyDispenser(true, 1, candies - 1)
-      case _ => CandyDispenser(false, coins + 1, candies)
+      case Turn => CandyDispenser(locked = true, 1, candies - 1)
+      case _    => CandyDispenser(locked = false, coins + 1, candies)
     }
 }
 
 object CandyDispenser {
-  def simulate(inputs: List[Input]): State[CandyDispenser, (Int, Int)] = for {
-    _ <- sequence(inputs map (i => modify[CandyDispenser](_ update i)))
-    s <- State.get
-  } yield (s.coins, s.candies)
+  def simulate(inputs: List[Input]): State[CandyDispenser, (Int, Int)] =
+    for {
+      _ <- sequence(inputs map (i => modify[CandyDispenser](_ update i)))
+      s <- State.get
+    } yield (s.coins, s.candies)
 }
