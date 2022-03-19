5b533da69fdeb070bcf2a4b355c03c0e9ec4b637
Roy Straub <roy@codecraftr.nl>
2022-03-19 11:45:25 +0100

    Sorted int list

--- src/main/scala/parttwo/parallelism/Par.scala
+++ src/main/scala/parttwo/parallelism/Par.scala
@@ -1,6 +1,5 @@
 package parttwo.parallelism
 
-import java.util.concurrent.ExecutorService
 import java.util.concurrent._
 
 object Par {
@@ -14,14 +13,22 @@ object Par {
   }
 
   def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
   def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
   def fork[A](a: => Par[A]): Par[A] = es => es.submit(() => a(es).get)
+
   def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
     (es: ExecutorService) => {
       val af = a(es)
       val bf = b(es)
       UnitFuture(f(af.get, bf.get))
     }
+
   def asyncF[A, B](f: A => B): A => Par[B] = (a: A) => lazyUnit(f(a))
+
+  def sortPar(parList: Par[List[Int]]): Par[List[Int]] =
+    map2(parList, unit(()))((n, _) => n.sorted)
 }
--- src/test/scala/parttwo/parallelism/ParTest.scala
+++ src/test/scala/parttwo/parallelism/ParTest.scala
@@ -2,9 +2,9 @@ package parttwo.parallelism
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
-import parttwo.parallelism.Par.Par
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import parttwo.parallelism.Par.{Par, sortPar}
+
+import java.util.concurrent.{ExecutorService, Executors}
 
 class ParTest extends AnyWordSpec with Matchers {
   private val executor: ExecutorService = Executors.newFixedThreadPool(2)
@@ -13,13 +13,24 @@ class ParTest extends AnyWordSpec with Matchers {
     "evaluate result asynchronously" in {
       val fun = (i: Int) => {
         val res = i.toString
-        println()
+        println(i)
         res
       }
 
-      val result = Par.asyncF(fun)(1)(executor)
+      val result = runMultiThreaded(Par.asyncF(fun)(1))
 
       result.get shouldBe "1"
     }
   }
+
+  "sortPar" should {
+    "sort list of ints" in {
+      val nums = Par.lazyUnit(List(9, 1, 7, 3, 5))
+      val result = runMultiThreaded(sortPar(nums))
+
+      result.get shouldBe List(1, 3, 5, 7, 9)
+    }
+  }
+
+  private def runMultiThreaded[A](par: Par[A]) = Par.run(executor)(par)
 }
