5c39706d91a49f5dbaee2dcb81073556f8949a52
Roy <roy@codecraftr.nl>
2022-04-25 11:03:25 +0200

    Weighted

--- src/main/scala/parttwo/testing/Gen.scala
+++ src/main/scala/parttwo/testing/Gen.scala
@@ -21,5 +21,11 @@ object Gen {
     State.sequence(List.fill(n)(g.sample))
   )
 
-  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = Gen.boolean.flatMap(if (_) g1 else g2)
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+    Gen.boolean.flatMap(if (_) g1 else g2)
+
+  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
+    val threshold = g1._2 / (g1._2.abs + g2._2.abs)
+    Gen(State(RNG.double)).flatMap(d => if (d < threshold) g1._1 else g2._1)
+  }
 }
